8184384c1fb9f88778636efd2cf5c7d6360a8d3d
junyi <47441677+immanuelhume@users.noreply.github.com>
2021-04-19 11:24:42 +0800

    added some stuff

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,16 +3,23 @@ sudo apt install zsh
 chsh -s $(which zsh)
 
 # install antigen
-curl -L git.io/antigen > antigen.zsh
+curl -L git.io/antigen > $HOME/antigen.zsh
 
 # install nvm + node LTS
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | zsh
-if [["$(command -v nvm)" == "nvm"]]; then
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | sh
+if [[ "$(command -v nvm)" == "nvm" ]]; then
     nvm install --lts
 else
-    echo "\u001b[31mnvm was not installed correctly\u001b[0m"
+    echo "nvm was not installed correctly"
 fi
+
+# install yarn
+npm install --global yarn
 
 # install poetry
 py=$(which python3 | which python)
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | $py - --no-modify-path
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | $py -
+
+# symlink .zshrc to $HOME
+rm -rf $HOME/.zshrc
+ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
