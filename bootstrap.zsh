bdc290542f3e05652a8c1ffc1506c14bb62e8741
junyi <47441677+immanuelhume@users.noreply.github.com>
2021-08-18 14:29:51 +0800

    refactor: split installing from bootstraping

--- bootstrap.zsh
+++ bootstrap.zsh
@@ -1,45 +1,28 @@
-#! /usr/bin/sh
+#! /usr/bin/zsh
+# Install stuff. Make use of .zshrc configs.
 set -e
-
-# install zsh
-sudo pacman -S zsh
-
-# install antigen
-curl -L git.io/antigen > $HOME/antigen.zsh
 
 # install nvm 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | sh
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+# install node LTS
+nvm install --lts
+# install yarn
+npm install --global yarn
 
 # install pyenv
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 sudo pacman -S --needed base-devel openssl zlib xz
 cd ~/.pyenv && src/configure && make -C src
-
-# symlink .zshrc to $HOME
-rm -rf $HOME/.zshrc
-ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
-
-zsh # run zsh
-
-# install node LTS
-if [[ "$(command -v nvm)" == "nvm" ]]; then
-    nvm install --lts
-else
-    echo "nvm was not installed correctly"
-fi
-
-# install yarn
-npm install --global yarn
-
-# install python
+# install python 3.8.5
 pyenv install 3.8.5
 pyenv global 3.8.5
+
+# install Golang
+sudo pacman -S go go-tools
 
 # ln vimfiles
 ln -s $HOME/dotfiles/vimfiles $HOME/.vim/vimfiles
 # and install vim plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-# change default shell to zsh (requires restart)
-chsh -s $(which zsh) 
